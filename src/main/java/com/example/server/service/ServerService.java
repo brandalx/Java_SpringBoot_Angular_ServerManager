138a12df7dfbdc6ee799b29789bf068e45ddfccf
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 18:49:38 +0400

    update: Server service vars

--- src/main/java/com/example/server/service/ServerService.java
+++ src/main/java/com/example/server/service/ServerService.java
@@ -1,4 +1,14 @@
 package com.example.server.service;
 
+import com.example.server.model.Server;
+
+import java.util.Collection;
+
 public interface ServerService {
+    Server create(Server server);
+    Collection<Server> list(int limit);
+    Server get(long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+
 }
