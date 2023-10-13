2545a3fcf2e45a90fb5d5c6ac7987ae6f0d6d2b3
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 18:58:13 +0400

    update: Server impl Added annotations: Lombok's @RequiredArgsConstructor for automatic constructor generation, Spring's @Service to designate the class as a service component, @Transactional for transactional behavior, and Lombok's @Slf4j for SLF4J logging support.

--- src/main/java/com/example/server/repo/ServerRepo.java
+++ src/main/java/com/example/server/repo/ServerRepo.java
@@ -4,6 +4,7 @@ import com.example.server.model.Server;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServerRepo extends JpaRepository<Server, Long> {
+
     Server findByIpAddress(String ipAddress);
     Server findByName(String name);
 
--- src/main/java/com/example/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/example/server/service/implementation/ServerServiceImplementation.java
@@ -1,11 +1,21 @@
 package com.example.server.service.implementation;
 
 import com.example.server.model.Server;
+import com.example.server.repo.ServerRepo;
 import com.example.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImplementation implements ServerService {
+    private final ServerRepo serverRepo;
+
     @Override
     public Server create(Server server) {
         return null;
