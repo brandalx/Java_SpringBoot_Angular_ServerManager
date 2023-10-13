1cff5aed5ac6c52711cbcdcb15b5c456d6f445f0
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 18:20:56 +0400

    Server class private attrs, status enumeration pkg

--- src/main/java/com/example/server/model/Server.java
+++ src/main/java/com/example/server/model/Server.java
@@ -1,2 +1,22 @@
-package com.example.server.model;public class Server {
+package com.example.server.model;
+
+import com.example.server.enumeration.Status;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Server {
+    private  Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
 }
