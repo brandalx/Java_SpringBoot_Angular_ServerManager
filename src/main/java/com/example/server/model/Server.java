bfb866343236982641115eac8d841aec34c449fe
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 18:36:33 +0400

    feat: Server attrubutes added

--- src/main/java/com/example/server/model/Server.java
+++ src/main/java/com/example/server/model/Server.java
@@ -1,8 +1,13 @@
 package com.example.server.model;
 
 import com.example.server.enumeration.Status;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import static jakarta.persistence.GenerationType.AUTO;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +17,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private  Long id;
+    @Column(unique = true)
+    @NotEmpty(message ="IP Address cannot be empty or null" )
     private String ipAddress;
     private String name;
     private String memory;
