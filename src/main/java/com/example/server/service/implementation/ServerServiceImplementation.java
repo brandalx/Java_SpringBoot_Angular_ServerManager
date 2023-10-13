1e31083497af20b94329ecbfee26d8d50207f2c2
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 18:51:33 +0400

    feat: Implementations added for server services

--- src/main/java/com/example/server/service/implementation/ServerServiceImplementation.java
+++ src/main/java/com/example/server/service/implementation/ServerServiceImplementation.java
@@ -1,4 +1,33 @@
 package com.example.server.service.implementation;
 
-public class ServerServiceImplementation {
+import com.example.server.model.Server;
+import com.example.server.service.ServerService;
+
+import java.util.Collection;
+
+public class ServerServiceImplementation implements ServerService {
+    @Override
+    public Server create(Server server) {
+        return null;
+    }
+
+    @Override
+    public Collection<Server> list(int limit) {
+        return null;
+    }
+
+    @Override
+    public Server get(long id) {
+        return null;
+    }
+
+    @Override
+    public Server update(Server server) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
 }
