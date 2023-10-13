156b407cf0427703981907852740ee4ee98a409c
brndalx <113251192+brandalx@users.noreply.github.com>
2023-10-13 18:52:39 +0400

    update: server ping for server service

--- src/main/java/com/example/server/service/ServerService.java
+++ src/main/java/com/example/server/service/ServerService.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
+    Server ping(String ipAdress);
     Collection<Server> list(int limit);
     Server get(long id);
     Server update(Server server);
